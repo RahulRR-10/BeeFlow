6e6f9d7c52c3e1412b5dbb79b35dad5fb2106275
RahulRR-10 <155348604+RahulRR-10@users.noreply.github.com>
2025-04-06 23:22:58 +0530

    Update gemini_config.dart

--- lib/config/gemini_config.dart
+++ lib/config/gemini_config.dart
@@ -1,5 +1,5 @@
 class GeminiConfig {
-  static const String apiKey = 'AIzaSyCh0vbpS56JzS5NhRegT1gbYUmBJy_YMPY';
+  static const String apiKey = 'API-KEY';
   static const String model = 'gemini-2.0-flash';
   static const String endpoint =
       'https://generativelanguage.googleapis.com/v1beta/models/$model:generateContent';
